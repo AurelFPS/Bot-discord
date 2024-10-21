f48986e809141198f45de3e051eba4178e44545c
AurelFPS <140328227+AurelFPS@users.noreply.github.com>
2024-10-21 19:09:28 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return "Le bot est en ligne"
 
 def run():
